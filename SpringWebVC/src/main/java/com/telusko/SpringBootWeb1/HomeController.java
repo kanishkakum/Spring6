95e1696f404a47537bf4c3751df514fffffc23ff
hp <kanishkakumar408@gmail.com>
2024-08-25 17:43:34 +0530

    model object, prefix and suffix for views

--- SpringWebVC/src/main/java/com/telusko/SpringBootWeb1/HomeController.java
+++ SpringWebVC/src/main/java/com/telusko/SpringBootWeb1/HomeController.java
@@ -1,6 +1,7 @@
 package com.telusko.SpringBootWeb1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,7 +13,7 @@ public class HomeController {
 	@RequestMapping("/")
 	public String home() {
 		System.out.println("Home method called");
-		return "index.jsp";
+		return "index";
 	}
 	
 	
@@ -20,12 +21,12 @@ public class HomeController {
 	
 	
 	@RequestMapping("add")
-	public String add(@RequestParam("num1") int num1,@RequestParam("num2") int num2, HttpSession session) {
+	public String add(@RequestParam("num1") int num1,@RequestParam("num2") int num2, Model model) {
 		
 		int result = num1+num2;
-		session.setAttribute("result", result);
+		model.addAttribute("result", result);
 		
-		return "result.jsp";
+		return "result";
 		
 	}
 
